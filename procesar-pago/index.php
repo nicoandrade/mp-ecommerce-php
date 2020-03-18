c33071aab26b3710a32c2649436597e4421aed9e
Nico Andrade <nicoandrade@gmail.com>
2020-03-18 17:09:31 -0300

    up

---
+++ procesar-pago/index.php
@@ -0,0 +1,37 @@
+<?php
+// Loading Composer
+require __DIR__ . '/vendor/autoload.php';
+
+// Mercado Pago Credentials
+MercadoPago\SDK::setAccessToken( "APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398" ); // On Production
+//MercadoPago\SDK::setAccessToken( "YOUR_TEST_ACCESS_TOKEN" ); // On Sandbox
+
+$file = 'logs.txt';
+
+$json_event = file_get_contents( 'php://input', true );
+$event = json_decode( $json_event );
+
+echo '<pre>';
+print_r( $event );
+echo '</pre>';
+echo '<pre>';
+print_r( $_POST );
+echo '</pre>';
+
+if ( isset( $event->type, $event->data->id ) ) {
+    $event_type = $event->type;
+    $event_id = $event->data->id;
+
+    $payment = MercadoPago\Payment . find_by_id( $_POST["id"] );
+
+    $current = file_get_contents( $file );
+    $current .= $event_type . " recibido - ID #" . $event_id . "\n";
+
+} else {
+    $current = file_get_contents( $file );
+    $current .= "Llamada directa\n";
+}
+
+file_put_contents( $file, $current );
+
+http_response_code( 200 );
